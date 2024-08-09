027cc6c69a574fc651a209168b1f022e213cc820
Michael Needham <needham.michael@epa.gov>
2024-08-09 15:17:19 -0400

    added setup.py to allow pip install of local package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='emiss_downscale',
+    version='0.0.1',
+    url='https://github.com/needham-michael/emiss_downscale',
+    author='Michael Needham',
+    author_email='needham.michael@epa.gov',
+    description='Methods to downscale photochemical model emissions from coarse to fine grids',
+    packages=find_packages(), 
+)
